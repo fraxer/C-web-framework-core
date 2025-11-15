6b8a995f101b550e34efa364acc497bd6e9926d5
fraxer <fraxer@mail.ru>
2025-11-15 23:31:37 +0700

    fix: compare length of strings in cmpstrn_lower

--- misc/helpers.h
+++ misc/helpers.h
@@ -3,7 +3,6 @@
 
 int helpers_mkdir(const char* path);
 int helpers_base_mkdir(const char* base_path, const char* path);
-int cmpstr(const char* a, const char* b);
 int cmpstr_lower(const char* a, const char* b);
 int cmpstrn_lower(const char* a, size_t a_length, const char* b, size_t b_length);
 char* create_tmppath(const char*);
