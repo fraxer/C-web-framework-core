1e033e746adb41ef606e1c0f60cd2c05f1329dce
fraxer <fraxer@mail.ru>
2025-11-15 18:55:58 +0700

    refactor: update storages3

--- framework/storage/storages3.h
+++ framework/storage/storages3.h
@@ -2,17 +2,19 @@
 #define __STORAGES3__
 
 #include "storage.h"
+#include "str.h"
 
 typedef struct {
     storage_t base;
-    char access_id[128];
-    char access_secret[128];
-    char protocol[6];
-    char host[128];
-    char port[6];
-    char bucket[128];
+    str_t access_id;
+    str_t access_secret;
+    str_t protocol;
+    str_t host;
+    str_t port;
+    str_t bucket;
+    str_t region;
 } storages3_t;
 
-storages3_t* storage_create_s3(const char* storage_name, const char* access_id, const char* access_secret, const char* protocol, const char* host, const char* port, const char* bucket);
+storages3_t* storage_create_s3(const char* storage_name, const char* access_id, const char* access_secret, const char* protocol, const char* host, const char* port, const char* bucket, const char* region);
 
 #endif
