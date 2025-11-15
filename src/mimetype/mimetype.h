db3fc64e826e74eb85812650c7011166525c6378
fraxer <fraxer@mail.ru>
2025-11-15 19:27:23 +0700

    fix

--- src/mimetype/mimetype.h
+++ src/mimetype/mimetype.h
@@ -14,7 +14,7 @@ typedef struct {
     map_t* table_type;  // mimetype -> set of extensions (string -> map_t*)
 } mimetype_t;
 
-mimetype_t* mimetype_create(size_t table_type_size, size_t table_ext_size);
+mimetype_t* mimetype_create(void);
 void mimetype_destroy(mimetype_t* mimetype);
 int mimetype_add(mimetype_t* mimetype, mimetype_table_type_t table_type, const char* key, const char* value);
 const char* mimetype_find_ext(mimetype_t* mimetype, const char* key);
